d9364810f22cba3fe682ee553dd34cc3d70cdef6
Daniel Vieites <dvieitest@gmail.com>
2025-08-14 08:44:33 +0200

    fix: bad require

--- lua/miniharp/init.lua
+++ lua/miniharp/init.lua
@@ -2,7 +2,7 @@
 local M = {}
 
 local state = require('miniharp.state')
-local util = require('miniharp.util')
+local utils = require('miniharp.utils')
 local core = require('miniharp.core')
 
 local function ensure_autosave()
@@ -13,8 +13,8 @@ local function ensure_autosave()
     vim.api.nvim_create_autocmd('BufLeave', {
         group = state.augroup,
         callback = function(args)
-            local file = util.bufname(args.buf); if file == '' then return end
-            local l, c = util.cursor(0)
+            local file = utils.bufname(args.buf); if file == '' then return end
+            local l, c = utils.cursor(0)
             core.update_last_for_file(file, l, c)
         end,
         desc = 'miniharp: remember last position for file marks',
