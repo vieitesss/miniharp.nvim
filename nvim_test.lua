f8b52ed62e94df9b60e612ce1bba0f34ca220587
Daniel Vieites <dvieitest@gmail.com>
2026-04-18 19:03:30 +0200

    test: add Neovim test init

---
+++ nvim_test.lua
@@ -0,0 +1,21 @@
+-- Test init file for miniharp.nvim development
+-- Add the plugin to runtime path
+vim.opt.runtimepath:prepend('.')
+
+vim.g.mapleader = ' '
+
+require('miniharp').setup({
+    autoload = true,
+    autosave = true,
+    show_on_autoload = false,
+})
+
+local miniharp = require('miniharp')
+
+vim.keymap.set('n', '<leader>ma', miniharp.toggle_file, { desc = 'miniharp: toggle file mark' })
+vim.keymap.set('n', '<C-n>', miniharp.next, { desc = 'miniharp: next file mark' })
+vim.keymap.set('n', '<C-p>', miniharp.prev, { desc = 'miniharp: previous file mark' })
+vim.keymap.set('n', '<leader>l', miniharp.show_list, { desc = 'miniharp: list marks' })
+vim.keymap.set('n', '<leader>ms', miniharp.save, { desc = 'miniharp: save marks' })
+vim.keymap.set('n', '<leader>mr', miniharp.restore, { desc = 'miniharp: restore marks' })
+vim.keymap.set('n', '<leader>mc', miniharp.clear, { desc = 'miniharp: clear marks' })
